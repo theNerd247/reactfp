5e0af160caa406ba141399f28bd2ab55de9ad5ab
theNerd247 <noah.harvey247 gm>
2019-09-12 20:31:58 -0400

    adds monoid

--- reader.js
+++ reader.js
@@ -1,7 +1,7 @@
 const asConst   = (a)     => (b)           => a
 const withConst = (x)     => (f)           => f(x)
 const c         = (...fs)           => (x) => fs.reduce((v, f) => f(v), x)
-const app       = (f)     => (x)           => f(x)
+const apply       = (f)     => (x)           => f(x)
 const flip      = (f)     => (a)    => (b) => f(a)(b)
 const fst       = (x)                      => x[0]
 const snd       = (x)                      => x[1]
@@ -13,6 +13,7 @@ const apn       = (ap)    => (f) => (...xs) => xs.reduce((g, x) => ap(g)(x), f)
 const Pair =
   { fmap : (f) => (p) => pair(f(fst(p)), snd(p))
   , bimap: (f) => (g) => (p) => pair(f(fst(p)), g(snd(p)))
+  , gmap : (f) => (p) => pair(fst(p), f(snd(p)))
   }
 
 const List =
@@ -22,22 +23,38 @@ const List =
 
 //Reader r a ~ (r -> a)
 const Reader =
-  { runReader     : app
+  { runReader     : apply
   , runReaderWith : withConst
-  , contramap     : (f) => (r) => c(f,r)
+  , fmap          : (f) => (r) => c(r,f)
   , bimap         : (f) => (g) => (r) => c(f, r, g)
   , pure          : asConst
+                //(r -> (a -> b)) -> (r -> a) -> (r -> b)
   , ap            : (f) => (r) => (x) => f(x)(r(x))
+  , ret           : pure
+  , bind          : (a) => (f) => c(a, f)
   }
 
 //State s a ~ (s -> (s, a))
 const State =
-  { runState     : app
+  { runState     : apply
   , runStateWith : withConst
   , fmap         : (f) => (s) => c(s, Pair.fmap(f))
   , bimap        : (f) => (g) => (s) => c(s, Pair.bimap(f)(g))
   , pure         : (x) => (s) => pair(x, s)
   , ap           : (sf) => (s) => c(sf, Reader.ap(c(fst, Pair.fmap))(c(snd, s)))
+  }
+
+//Coreader s a ~ (a, s)
+const Writer = (m) =>
+  { runCoreader : apply
+  , runCoreader : withConst
+  , fmap : Pair.fmap
+  , pure : (x) => pair(x, m.empty)
+  , ap   : (fw) => (r) => withConst (Pair.fmap(fst(fw))(r)) (gmap(m.plus(snd(fw))))
+  }
+
+const Monoid = (f, m) => 
+  { mconcat = f.foldr m.append m.mempty
   }
 
 const plus = (a) => (b) => a + b
